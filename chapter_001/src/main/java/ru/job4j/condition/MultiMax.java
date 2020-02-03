4f63b56c36c6e1cdd00c3ba8260b1f87b57a0b1a
CoffeeTurbo@gmail.com <coffeeturbo@gmail.com>
2020-02-03 22:54:26 +0300

    505 MultiMax

--- chapter_001/src/main/java/ru/job4j/condition/MultiMax.java
+++ chapter_001/src/main/java/ru/job4j/condition/MultiMax.java
@@ -3,10 +3,9 @@ package ru.job4j.condition;
 public class MultiMax {
     public int max(int first, int second, int third) {
 
-        int result = first > second ? first : second;
+        int result;
+        result = first > second ? first : second;
 
-        result = result > third ? result : third ;
-
-        return result;
+        return result > third ? result : third ;
     }
 }
