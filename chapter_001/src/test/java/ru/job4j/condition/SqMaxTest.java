06bd020d116bc3455405418cd6cea850b45093cc
CoffeeTurbo@gmail.com <coffeeturbo@gmail.com>
2020-02-04 15:55:33 +0300

    506 SqMax add more tests

--- chapter_001/src/test/java/ru/job4j/condition/SqMaxTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/SqMaxTest.java
@@ -9,9 +9,33 @@ import static org.junit.Assert.*;
 public class SqMaxTest {
 
     @Test
-    public void whenMax() {
+    public void whenMaxFirst() {
+        SqMax check = new SqMax();
+        int result = check.max(7, 6, 4, 5);
+
+        assertThat(result, is(7));
+    }
+
+    @Test
+    public void whenMaxSecond() {
+        SqMax check = new SqMax();
+        int result = check.max(6, 7, 4, 5);
+
+        assertThat(result, is(7));
+    }
+
+    @Test
+    public void whenMaxThid() {
         SqMax check = new SqMax();
         int result = check.max(6, 4, 7, 5);
+
+        assertThat(result, is(7));
+    }
+
+    @Test
+    public void whenMaxFourth() {
+        SqMax check = new SqMax();
+        int result = check.max(6, 4, 5, 7);
 
         assertThat(result, is(7));
     }
