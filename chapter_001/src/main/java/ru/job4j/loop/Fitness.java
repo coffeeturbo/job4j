a12ac19cedd380dcfa8f55aec474b1f62cbca1ea
CoffeeTurbo@gmail.com <coffeeturbo@gmail.com>
2020-02-05 21:56:22 +0300

    606 Fitnes

---
+++ chapter_001/src/main/java/ru/job4j/loop/Fitness.java
@@ -0,0 +1,21 @@
+package ru.job4j.loop;
+
+public class Fitness {
+    public static int calc(int ivan, int nik) {
+        int month = 0;
+
+        int ivanProgressK = 3;
+        int nikProgressK = 2;
+
+        while (ivan < nik){
+
+            ivan *= ivanProgressK;
+            nik *= nikProgressK;
+
+            month++;
+        }
+
+        return month;
+    }
+
+}
