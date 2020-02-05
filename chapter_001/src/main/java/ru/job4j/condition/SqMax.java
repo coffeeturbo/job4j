a6f19661e12663ed5f9409049e4646512c62ddd0
CoffeeTurbo@gmail.com <coffeeturbo@gmail.com>
2020-02-05 12:18:55 +0300

    506 fixx

--- chapter_001/src/main/java/ru/job4j/condition/SqMax.java
+++ chapter_001/src/main/java/ru/job4j/condition/SqMax.java
@@ -4,24 +4,18 @@ public class SqMax {
     public static int max(int first, int second, int third, int forth) {
         int result = forth;
         if (first > second) {
-            result = first;
             if (first > third) {
-                result = first;
                 if (first > forth) {
                     result = first;
-                } else {
-                    result = forth;
                 }
             } else {
                 result = third;
             }
         } else if (second > third) {
-            result = second;
             if (second > forth) {
                 result = second;
             }
         } else if (third > forth) {
-
             result = third;
         }
         return result;
