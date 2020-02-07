a907ba0281ad7b31da9385bdf37fab3a91afd2d9
CoffeeTurbo@gmail.com <coffeeturbo@gmail.com>
2020-02-07 17:19:09 +0300

    609 Mortgage

---
+++ chapter_001/src/main/java/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,19 @@
+package ru.job4j.loop;
+
+public class Mortgage {
+
+    public int year(int amount, int salary, double percent) {
+        int year = 0;
+
+        double percentK = ((percent/100) + 1);
+
+        while (amount > 0)
+        {
+            double debet = amount * percentK;
+            amount = (int) debet - salary;
+            year++;
+        }
+
+        return year;
+    }
+}
