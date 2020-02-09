9a075976c2d782f7857378287345ac70ea98a3c4
CoffeeTurbo@gmail.com <coffeeturbo@gmail.com>
2020-02-09 12:21:27 +0300

    702 array filling

--- chapter_001/src/main/java/ru/job4j/array/ArrayDefinition.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -1,9 +1,16 @@
 package ru.job4j.array;
 
 public class ArrayDefinition {
-    public static void main(String[] args ) {
-        short[] ages = new short[10];
-        String[] surnames = new String[100500];
-        float[] prices = new float[40];
+    public static void main(String[] args) {
+        String[] names = new String[4];
+
+        names[0] = "Senior Cartman";
+        names[1] = "Eric Cartman";
+        names[2] = "The Coon";
+        names[3] = "Senior Eric Cartmenes";
+
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
+        }
     }
 }
