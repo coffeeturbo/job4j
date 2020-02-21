c6e2e7c1cca5726201596a0e5f40d2d659bcc60e
CoffeeTurbo@gmail.com <coffeeturbo@gmail.com>
2020-02-21 11:27:56 +0300

    2.2. Рефакторинг - Площадь треугольника.[#230252]

--- chapter_001/src/main/java/ru/job4j/condition/TrgArea.java
+++ chapter_001/src/main/java/ru/job4j/condition/TrgArea.java
@@ -2,17 +2,46 @@ package ru.job4j.condition;
 
 public class TrgArea {
 
-    public static double area(double a, double b, double c) {
+    private Point first, second, third;
 
-        double p = (a + b + c) / 2;
 
-        double area = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+
+    public static double period(double a, double b, double c) {
+        return (a + b + c) / 2;
+    }
+
+    public static double area(int x1, int y1, int x2, int y2, int x3, int y3) {
+
+        Point first = new Point(x1, y1);
+        Point second = new Point(x2, y2);
+        Point third = new Point(x3, y3);
+
+        double a, b, c;
+        a = first.distance(second);
+        b = second.distance(third);
+        c = third.distance(first);
+
+        double p = period(a, b, c);
+
+        double area = -1;
+        if (exist(a, b, c)) {
+            area = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+        }
 
         return area;
     }
 
+    public static boolean exist(double ab, double ac, double bc) {
+
+        boolean sideA = ab + ac > bc;
+        boolean sideB = ab + bc > ac;
+        boolean sideC = ac + bc > ab;
+
+        return sideA && sideB && sideC;
+    }
+
     public static void main(String[] args) {
-        double rsl = TrgArea.area(2, 2, 2);
+        double rsl = TrgArea.area(0, 0, 0, 3, 3, 0);
         System.out.println("area (2, 2, 2) = " + rsl);
     }
 
--- chapter_001/src/main/java/ru/job4j/condition/Triangle.java
+++ chapter_001/src/main/java/ru/job4j/condition/Triangle.java
@@ -1,6 +1,35 @@
 package ru.job4j.condition;
 
 public class Triangle {
+    private Point first, second, third;
+
+    public Triangle(Point first, Point second, Point third) {
+        this.first = first;
+        this.second = second;
+        this.third = third;
+    }
+
+    public static double period(double a, double b, double c) {
+        return (a + b + c) / 2;
+    }
+
+    public double area() {
+
+        double a, b, c;
+        a = first.distance(second);
+        b = second.distance(third);
+        c = third.distance(first);
+
+        double p = period(a, b, c);
+
+        double area = -1;
+        if (exist(a, b, c)) {
+            area = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+        }
+
+        return area;
+    }
+
     public static boolean exist(double ab, double ac, double bc) {
 
         boolean sideA = ab + ac > bc;
@@ -8,5 +37,10 @@ public class Triangle {
         boolean sideC = ac + bc > ab;
 
         return sideA && sideB && sideC;
+    }
+
+    public static void main(String[] args) {
+        double rsl = TrgArea.area(0, 0, 0, 3, 3, 0);
+        System.out.println("area (2, 2, 2) = " + rsl);
     }
 }
