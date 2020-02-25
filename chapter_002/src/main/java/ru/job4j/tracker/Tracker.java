0f7555a9b45db52d24880fdea9e8b2b38fc178fc
CoffeeTurbo@gmail.com <coffeeturbo@gmail.com>
2020-02-25 11:11:00 +0300

    5. Реализовать класс Tracker[#230279]

--- chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
+++ chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,4 +1,85 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Tracker {
+    private final Item[] items = new Item[100];
+    private int position = 0;
+
+    /**
+     * Add item.
+     *
+     * @param item the item
+     * @return the item
+     */
+    public Item add(Item item) {
+        item.setId(this.generateId());
+        items[this.position++] = item;
+        return item;
+    }
+
+
+    /**
+     * Find all item [ ].
+     *
+     * @return the item [ ]
+     */
+    public Item[] findAll() {
+        Item[] itemsWithoutNull = new Item[items.length];
+        int size = 0;
+        for (int index = 0; index < position; index++) {
+            Item item = items[index];
+            if (item != null) {
+                itemsWithoutNull[size] = item;
+                size++;
+            }
+        }
+        return Arrays.copyOf(itemsWithoutNull, size);
+    }
+
+    /**
+     * Find by name item [ ].
+     *
+     * @param key the key
+     * @return the item [ ]
+     */
+    public Item[] findByName(String key) {
+        int size = 0;
+        Item[] itemsByNames = new Item[position];
+        for (int i = 0; i < position; i++) {
+            if (items[i].getName().equals(key)) {
+                itemsByNames[size] = items[i];
+                size++;
+            }
+        }
+        return Arrays.copyOf(itemsByNames, size);
+    }
+
+
+    /**
+     * Find by id item.
+     *
+     * @param id the id
+     * @return the item
+     */
+    public Item findById(String id) {
+        Item item = null;
+        for (int i = 0; i < position; i++) {
+            if (items[i].getId().equals(id)) {
+                item = items[i];
+            }
+        }
+        return item;
+    }
+
+    /**
+     * Метод генерирует уникальный ключ для заявки.
+     * Так как у заявки нет уникальности полей, имени и описание. Для идентификации нам нужен уникальный ключ.
+     * @return Уникальный ключ.
+     */
+    private String generateId() {
+        Random rm = new Random();
+        return String.valueOf(rm.nextLong() + System.currentTimeMillis());
+    }
 }
---
+++ chapter_002/src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -0,0 +1,51 @@
+package ru.job4j.tracker;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.is;
+
+public class TrackerTest {
+    @Test
+    public void whenAddNewItemThenTrackerHasSameItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("test1");
+        tracker.add(item);
+        Item result = tracker.findById(item.getId());
+        assertThat(result.getName(), is(item.getName()));
+    }
+
+    @Test
+    public void whenFindAllItemsTrackerHasTwoItems() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("test1");
+        tracker.add(item);
+        Item item2 = new Item("test2");
+        tracker.add(item2);
+        Item[] result = tracker.findAll();
+        assertThat(result[0].getName(), is(item.getName()));
+        assertThat(result[1].getName(), is(item2.getName()));
+    }
+
+    @Test
+    public void whenFindByNameItemsTrackerHasTwoItems() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("test1");
+        tracker.add(item);
+        Item item2 = new Item("test2");
+        tracker.add(item2);
+        Item[] result = tracker.findByName(item2.getName());
+        assertThat(result[0].getName(), is(item2.getName()));
+    }
+
+    @Test
+    public void whenFindByIdItemTrackerHasTwoItems() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("test1");
+        tracker.add(item);
+        Item item2 = new Item("test2");
+        tracker.add(item2);
+        Item result = tracker.findById(item.getId());
+        assertThat(result.getName(), is(item.getName()));
+    }
+}
