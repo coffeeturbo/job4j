9eaa5a2ed4489cfe299e95ec670f50a6b357774d
CoffeeTurbo@gmail.com <coffeeturbo@gmail.com>
2020-03-24 21:28:30 +0300

    0. Коллекции, ArrayList, List, Обобщения[#230195] review fix

--- chapter_002/src/main/java/ru/job4j/collection/UsageArrayList.java
+++ chapter_002/src/main/java/ru/job4j/collection/UsageArrayList.java
@@ -6,9 +6,9 @@ public class UsageArrayList {
     public static void main(String[] args) {
         ArrayList<String> names = new ArrayList<String>();
 
-        names.add(0, "Petr");
-        names.add(0, "Ivan");
-        names.add(0, "Stepan");
+        names.add("Petr");
+        names.add("Ivan");
+        names.add("Stepan");
 
         for (String name : names) {
             System.out.println(name);
