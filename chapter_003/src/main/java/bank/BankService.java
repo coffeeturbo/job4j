c26e743d3ca804a9d2db00dc715ece71f2a20b32
CoffeeTurbo@gmail.com <coffeeturbo@gmail.com>
2020-03-26 17:06:30 +0300

    3. Банковские переводы.[#230199] review fix

--- chapter_003/src/main/java/bank/BankService.java
+++ chapter_003/src/main/java/bank/BankService.java
@@ -10,17 +10,12 @@ public class BankService {
 
     public void addUser(User user) {
         List<Account>  accountList = new ArrayList<>();
-        accountList.add(new Account("1232313", 0));
-        users.put(user, accountList);
+        users.putIfAbsent(user, accountList);
     }
 
     public void addAccount(String passport, Account account) {
-        for (User user : users.keySet()) {
-            if (user.getPassport().equals(passport)) {
-                users.get(user).add(account);
-                break;
-            }
-        }
+        User user = findByPassport(passport);
+        users.get(user).add(account);
     }
 
     public User findByPassport(String passport) {
@@ -38,17 +33,17 @@ public class BankService {
 
     public Account findByRequisite(String passport, String requisite) {
         Account result = null;
-        for (User user : users.keySet()) {
-            if (user.getPassport().equals(passport)) {
-                List<Account> accounts = users.get(user);
-                for (Account account : accounts) {
-                    if (account.getRequisite().equals(requisite)) {
-                        result = account;
-                        break;
-                    }
+        User user = findByPassport(passport);
+        List<Account> accounts = users.get(user);
+        if (accounts != null) {
+            for (Account account : accounts) {
+                if (account.getRequisite().equals(requisite)) {
+                    result = account;
+                    break;
                 }
             }
         }
+
         return result;
     }
 
@@ -60,7 +55,10 @@ public class BankService {
         boolean rsl = false;
         Account source = findByRequisite(srcPassport, srcRequisite);
         Account target = findByRequisite(destPassport, destRequisite);
-        if (source != null && target != null) {
+        if (source != null
+                && target != null
+                && source.getBalance() >= amount
+        ) {
             source.setBalance(source.getBalance() - amount);
             target.setBalance(target.getBalance() + amount);
             rsl = true;
