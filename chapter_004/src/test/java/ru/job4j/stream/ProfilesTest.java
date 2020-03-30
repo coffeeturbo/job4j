a1f0c44823c4f2ab4bfb46b2f629f96d5aaf730a
CoffeeTurbo@gmail.com <coffeeturbo@gmail.com>
2020-03-30 21:27:54 +0300

    2. Список адресов[#230056]

---
+++ chapter_004/src/test/java/ru/job4j/stream/ProfilesTest.java
@@ -0,0 +1,29 @@
+package ru.job4j.stream;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class ProfilesTest {
+
+    private List<Profile> profiles = List.of(
+            new Profile(new Address("Москва", "1",  1, 1)),
+            new Profile(new Address("London", "2",  2, 2)),
+            new Profile(new Address("New york", "3",  3, 3))
+    );
+
+    @Test
+    public void collect() {
+        List<Address> actual =  Profiles.collect(profiles);
+
+        List<Address> exprected = List.of(
+                new Address("Москва", "1",  1, 1),
+                new Address("London", "2",  2, 2),
+                new Address("New york", "3",  3, 3)
+        );
+
+        assertEquals(exprected, actual);
+    }
+}
