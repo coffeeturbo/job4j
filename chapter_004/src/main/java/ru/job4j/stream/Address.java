571492da3978334971de71ad6cda1073f8697fb1
CoffeeTurbo@gmail.com <coffeeturbo@gmail.com>
2020-03-30 21:42:33 +0300

    3. Уникальность элементов и сортировка.[#230057]

--- chapter_004/src/main/java/ru/job4j/stream/Address.java
+++ chapter_004/src/main/java/ru/job4j/stream/Address.java
@@ -2,7 +2,7 @@ package ru.job4j.stream;
 
 import java.util.Objects;
 
-public class Address {
+public class Address implements Comparable<Address> {
     private String city;
     private String street;
 
@@ -43,5 +43,10 @@ public class Address {
     @Override
     public int hashCode() {
         return Objects.hash(city, street, home, apartment);
+    }
+
+    @Override
+    public int compareTo(Address o) {
+        return city.compareTo(o.city);
     }
 }
--- chapter_004/src/test/java/ru/job4j/stream/ProfilesTest.java
+++ chapter_004/src/test/java/ru/job4j/stream/ProfilesTest.java
@@ -2,6 +2,7 @@ package ru.job4j.stream;
 
 import org.junit.Test;
 
+import java.util.Comparator;
 import java.util.List;
 
 import static org.junit.Assert.*;
@@ -10,7 +11,10 @@ public class ProfilesTest {
 
     private List<Profile> profiles = List.of(
             new Profile(new Address("Москва", "1",  1, 1)),
+            new Profile(new Address("Москва", "1",  1, 1)),
             new Profile(new Address("London", "2",  2, 2)),
+            new Profile(new Address("London", "2",  2, 2)),
+            new Profile(new Address("New york", "3",  3, 3)),
             new Profile(new Address("New york", "3",  3, 3))
     );
 
@@ -26,4 +30,21 @@ public class ProfilesTest {
 
         assertEquals(exprected, actual);
     }
+
+
+    @Test
+    public void sort() {
+        List<Address> actual =  Profiles.collect(profiles);
+
+        List<Address> exprected = List.of(
+                new Address("London", "2",  2, 2),
+                new Address("New york", "3",  3, 3),
+                new Address("Москва", "1",  1, 1)
+        );
+
+        Comparator<Address> comparator = Address::compareTo;
+        actual.sort(comparator);
+        assertEquals(exprected, actual);
+    }
+
 }
