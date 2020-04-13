d0912a4f90111ac2717c49f3f944e3672ed19d46
CoffeeTurbo@gmail.com <coffeeturbo@gmail.com>
2020-04-13 13:31:39 +0300

    Отсортировать департаменты[#230180] review fix 6

--- chapter_003/src/main/java/collection/departments/DepDescComp.java
+++ chapter_003/src/main/java/collection/departments/DepDescComp.java
@@ -5,15 +5,10 @@ import java.util.Comparator;
 public class DepDescComp implements Comparator<String> {
     @Override
     public int compare(String o1, String o2) {
-        var rsl = 0;
         String[] left = o1.split("/");
         String[] right = o2.split("/");
+
         var comp = right[0].compareTo(left[0]);
-        if (comp == 0) {
-            rsl = o1.compareTo(o2);
-        } else {
-            rsl = o2.compareTo(o1);
-        }
-        return rsl;
+        return comp == 0 ? o1.compareTo(o2) : comp;
     }
 }
